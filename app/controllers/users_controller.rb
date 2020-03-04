92ae6fc27ed64ee2e887dd848a1053d3041372e8
Ellie Mitchell <boromirthebold@gmail.com>
2020-03-04 12:43:57 -0500

    Commit with routing issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,22 @@
 class UsersController < ApplicationController
   skip_before_action :authorized, only: [:new, :create]
   
-  def index
-    @users = User.all
-  end
-  
-  def new
-    @user = User.new
+  def create
+    @user = User.new(user_params)
+   session[:user_id] = @user.id
+   redirect_to '/welcome'
   end
   
   def show
     @user = User.find(params[:id])
   end
   
+  def index
+    @users = User.all
+  end
   
-  def create
-    @user = User.new(user_params)
-   session[:user_id] = @user.id
-   redirect_to '/welcome'
+  def new
+    @user = User.new
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
    post 'login', to: 'sessions#create'
    get 'welcome', to: 'sessions#welcome'
    get 'authorized', to: 'sessions#page_requires_login'
+   get '/users', to: 'users#index'
    root 'welcome#index'
 end
