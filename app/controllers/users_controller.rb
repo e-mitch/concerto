0e02101b6ec9430aee4443b41597f236b700a9a8
Ellie Mitchell <boromirthebold@gmail.com>
2020-03-04 12:29:15 -0500

    stuck on user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  skip_before_action :authorized, only: [:new, :create]
+  
+  def index
+    @users = User.all
   end
   
-  def create
+  def new
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  
+  def create
+    @user = User.new(user_params)
+   session[:user_id] = @user.id
+   redirect_to '/welcome'
+  end
+  
+  private
+   def user_params
+     params.require(:user).permit(:fname, :lname, :email, :username,        
+   :password)
+   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+   resources :users, only: [:new, :create]
+   get 'login', to: 'sessions#new'
+   post 'login', to: 'sessions#create'
+   get 'welcome', to: 'sessions#welcome'
+   get 'authorized', to: 'sessions#page_requires_login'
+   root 'welcome#index'
 end
